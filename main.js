a7f08c0dbfe64dcc852999a95f9c289651228a45
IchBinSmart <58534469+IchBinSmart@users.noreply.github.com>
2020-01-01 20:25:50 +0100

    Update main.js

--- main.js
+++ main.js
@@ -556,7 +556,7 @@ function getLog(callback, i) {
 
 function isEquivalent(a, b) {
     // Create arrays of property names
-    const aProps = Object.getOwnPropertyNames(a);fan_
+    const aProps = Object.getOwnPropertyNames(a);
     const bProps = Object.getOwnPropertyNames(b);
 
     // If number of properties is different,
